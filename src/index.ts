00a8b9747b39e8269fb35dd8d83e2c0c72a31661
pino._.hoo <inhoo23@naver.com>
2022-09-24 21:46:31 +0900

    시퀄라이즈 셋팅 + 폴더 구조(1)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express from "express"
+import sequelize from "./models"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-const port = 8002;
-app.listen(port, async () => console.log(`SERVER ON! PORT : ${port}`));
+app.use("/api", require("./api"))
+
+const port = 8002
+app.listen(port, async () => {
+  console.log(`SERVER ON SUCCESS! PORT : ${port}`)
+  await sequelize
+    .authenticate()
+    .then(async () => {
+      console.log("DATABASE CONNECTION SUCCESS")
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+})
