1fb080fa25e8f91ca6c11f20481a150bea1bb582
pino._.hoo <inhoo23@naver.com>
2022-09-24 21:56:13 +0900

    cors.cookie-parser 생성

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import sequelize from "./models"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+app.use(cookieParser())
 
 app.use("/api", require("./api"))
 
