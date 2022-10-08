941cd78e131128c5ccbd9040364c85eb6f53f19d
unknown <sung min oh>
2022-10-08 21:45:50 +0900

    passport & jwt 구현 테스트 완

--- src/api/board_count/index.ts
+++ src/api/board_count/index.ts
@@ -2,9 +2,9 @@ const router = require("express").Router()
 const ctrl = require("./controller")
 
 //조회수 증가
-router.post("/:board_id", ctrl.process.plus);
-//조회수 조회
-router.get("/:board_id", ctrl.process.read);
+// router.post("/:board_id", ctrl.process.plus);
+// //조회수 조회
+// router.get("/:board_id", ctrl.process.read);
 
 
 module.exports = router
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -46,28 +46,25 @@ const process = {
       //기본적으로 인증에 성공하면 session을 생성함
       passport.authenticate('local', { session: false }, (err, user) => {
         // passport-local 결과에서 Err가 있거나, 유저가 없을 때
-        if (err) return res.status(400).json({message: `로그인 중 err발생 => ${err}`})
-        else if (!user) return res.redirect('/login')
+        if (err || !user) {
+          return res.status(400).json({
+            message: 'Something is not right',
+            user: user,
+          })
+        }
         // 로그인 후 코드 작성
         req.login(user, { session: false }, (err) => {
           // err 있을 때 처리
           if (err) {
-            console.log("passport를 이용하여 login을 처리하는 중 err발생 => " +err)
-            return res.status(400).json({ message : `passport를 이용하여 login을 처리하는 중 err발생 =>${err}`})
+            console.log(err)
+            res.send(err)
           }
           // jwt를 이용하여 token을 생성
-          //123은 시크릿키 바꿔야함
           const token = jwt.sign({ idx: user.idx }, '123')
           
           // cookie에 accessToken에 token을 담아서 저장
           res.cookie('accessToken', token, {
             expires: new Date(Date.now() + 86400e3),
-            /*보안정책상 가장 강력
-            * 강력 -> 위험
-            * strict
-            * lax
-            * none
-            * */
             sameSite: 'strict',
           })
           
@@ -76,28 +73,28 @@ const process = {
         })
       })(req, res)
 
-      const { email, password } = req.body
-      //db에 저장된 패스워드를 찾음
-      let dbPassword:string = "";
-      let dbSalt:string = "";
-      await User.findAll({ 
-        attributes : ['salt', 'password'],
-        where:{email : email} 
-      }).then(v => {
-        dbPassword = v[0].password;
-        dbSalt = v[0].salt;
-      }).then(v=> console.log("db에서 값 정상적으로 가져옴"))
-      .catch(v => {
-        res.status(400).json({message : `db에서 값을 가져오는 중 에러 발생 => \n${v}`})
-      })
-      //변수에 값이 저장되지 않았다면 오류를 반환
-      if(!dbPassword) return res.status(409).json({
-        message : "db에 해당하는 이메일이 존재하지 않음"
-      });
-      //대조를 위해서 password를 만듦
-      const createPassword = await createHashPassword(password, dbSalt, 64, 14582);
-      if(dbPassword === createPassword) return res.status(200).json({message : "로그인 성공"});
-      res.status(400).json({message : "비밀번호가 틀립니다"})
+    //   const { email, password } = req.body
+    //   //db에 저장된 패스워드를 찾음
+    //   let dbPassword:string = "";
+    //   let dbSalt:string = "";
+    //   await User.findAll({ 
+    //     attributes : ['salt', 'password'],
+    //     where:{email : email} 
+    //   }).then(v => {
+    //     dbPassword = v[0].password;
+    //     dbSalt = v[0].salt;
+    //   }).then(v=> console.log("db에서 값 정상적으로 가져옴"))
+    //   .catch(v => {
+    //     res.status(400).json({message : `db에서 값을 가져오는 중 에러 발생 => \n${v}`})
+    //   })
+    //   //변수에 값이 저장되지 않았다면 오류를 반환
+    //   if(!dbPassword) return res.status(409).json({
+    //     message : "db에 해당하는 이메일이 존재하지 않음"
+    //   });
+    //   //대조를 위해서 password를 만듦
+    //   const createPassword = await createHashPassword(password, dbSalt, 64, 14582);
+    //   if(dbPassword === createPassword) return res.status(200).json({message : "로그인 성공"});
+    //   res.status(400).json({message : "비밀번호가 틀립니다"})
     } catch (err) {
       res.status(400).json({ message: "BAD_REQUEST" })
     }
